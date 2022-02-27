89b35e5ca179d3f7772bf856ca94eb6db71fac81
CodeWithSouma <souma.hitech@gmail.com>
2022-02-27 13:54:16 +0530

    added two more dummy data in train.js

--- reactapp/src/components/context/Train.js
+++ reactapp/src/components/context/Train.js
@@ -30,5 +30,21 @@ export default[
         time:"12pm",
         price:"500",
         img:"https://i.pinimg.com/originals/74/f2/1e/74f21ee5c78141592ac50e2fdbd967ce.jpg"
+    },
+    {
+        id:"grid5",
+        trainName:"chennai Exp.",
+        place:"chennai to mumbai",
+        time:"12pm",
+        price:"500",
+        img:"https://images.newindianexpress.com/uploads/user/imagelibrary/2021/4/12/w900X450/Train.jpg?w=400&dpr=2.6"
+    },
+    {
+        id:"grid3",
+        trainName:"Janshatabdi Exp.",
+        place:"Howrah to Patna",
+        time:"12pm",
+        price:"500",
+        img:"https://media.istockphoto.com/photos/passenger-train-india-picture-id1189029599?k=20&m=1189029599&s=612x612&w=0&h=XWf7Baa12jYKM25AiypyFfLGc7TD8xc6vYYih5qOr6I="
     }
 ]
