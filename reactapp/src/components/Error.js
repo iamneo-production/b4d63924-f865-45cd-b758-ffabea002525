9fb3d2e4248c7660165dcb11e574900a3497ce08
prakash-20 <prakashkathirvel20@gmail.com>
2022-03-09 21:29:18 +0530

    Added Routing and admin dashboard

--- reactapp/src/components/Error.js
+++ reactapp/src/components/Error.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { BiErrorAlt } from "react-icons/bi";
-import './../index.css';
+import "./../index.css";
 const Error = () => {
   return (
-      <div className='d-flex flex-column justify-content-center align-items-center mt-5'>
-        <h1>Oops Error < BiErrorAlt className="text-danger"/></h1>
-        <Link to="/user/dashboard" className='btn nav-bg text-white'>Go Back</Link>
+    <div className="d-flex flex-column justify-content-center align-items-center mt-5">
+      <h1>
+        Oops Error <BiErrorAlt className="text-danger" />
+      </h1>
+      <Link to="/user/dashboard" className="btn nav-bg text-white">
+        Go Back
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Error;
---
+++ reactapp/src/components/admin_components/AddVehicle.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "../../index.css";
+// import Traincard from "./Traincard";
+// import Searchcomponent from "./Searchcomponent";
+// import { traindata } from "./Train";
+
+const AddVehicle = () => {
+  return (
+    <>
+      <Navbar />
+      <div id="dashBoard" className="container-fluid bg-user-dashboard py-3">
+        Add Vehicle
+      </div>
+    </>
+  );
+};
+
+export default AddVehicle;
