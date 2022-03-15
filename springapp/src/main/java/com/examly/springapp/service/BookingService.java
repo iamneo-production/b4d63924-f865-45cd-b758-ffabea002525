2f05d006591b9123a6adac09596f4cf23ad98a02
Uma Nagasree Anjana Sowjanya Kalvakolanu <kunsasowjanya@gmail.com>
2022-03-15 11:17:13 +0530

    I have completed BOoking and Passenger modules

--- springapp/src/main/java/com/examly/springapp/service/BookingService.java
+++ springapp/src/main/java/com/examly/springapp/service/BookingService.java
@@ -5,6 +5,7 @@ import com.examly.springapp.entity.Passenger;
 import com.examly.springapp.entity.User;
 import com.examly.springapp.entity.Vehicle;
 import com.examly.springapp.repository.BookingRepository;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -28,26 +30,41 @@ public class BookingService {
     }
 
     @Transactional
-    public Booking createBooking(int vehicleId, Booking booking, List<Passenger> listOfPassenger){
-        //TODO: at first we have to check this valid vehicle or not and vehicle is available or not
+    public Booking createBooking(int vehicleId, Booking booking){
+
+        // check if vehicle with given vehicleId is existing in database or not
         Optional<Vehicle> vehicleOptional = vehicleService.findById(vehicleId);
-        if (vehicleOptional.isEmpty()) throw new IllegalStateException("this vehicle id is not present");
+        if (vehicleOptional.isEmpty()) throw new IllegalStateException("Vehicle is not present");
+
+        // if vehicle exists check for its available status
         Vehicle vehicle = vehicleOptional.get();
-        if (!vehicle.getAvailableStatus().equalsIgnoreCase("Available")) throw new IllegalStateException("this vehicle is not available to book");
+        if (!vehicle.getAvailableStatus().equalsIgnoreCase("Available")) throw new IllegalStateException(vehicle.getName()+" is not available to book");
 
-        //TODO: vehicle sit is available or not we have to check
-        if (booking.getNumberOfPassanger() != listOfPassenger.size()) throw new IllegalStateException("number of passenger and list of passenger is not matched");
+        // check if no.of passengers and size of passenger list passed are equal or not
+        if (booking.getNumberOfPassanger() != booking.getPassengers().size()) throw new IllegalStateException("Number of passenger and list of passenger is not matched");
 
-        //TODO: first we have to fetch current logged in user then we can create booking
-        int currentUserId = 1;
+        // check for passanger capacity
+        if (booking.getNumberOfPassanger() > vehicle.getCapacity()) throw new IllegalStateException("Passenger capacity exceeded");
+
+        //fetch current logged user and passanger list, then create booking
+        User currentLoggedInUser = applicationUserDetailsService.getCurrentLoggedInUser();
+        Set<Passenger> passangers = booking.getPassengers();
+
+        // registering user, vehicle and passangers on current booking
         booking.setVehicle(vehicle);
-        //booking.setUser();
+        booking.setUser(currentLoggedInUser);
+        booking.setPassengers(passangers);
+
+        // registering current booking on each passanger
+        for (Passenger passanger : passangers){
+            passanger.setBooking(booking);
+        }
+
         Booking newBooking = bookingRepository.save(booking);
 
-        for (Passenger passenger : listOfPassenger){
-            passenger.setBooking(newBooking);
-            passengerService.save(passenger);
-        }
+        // modifying vehicle capacity and available status
+        modifyVehicleCapacity(newBooking, "createBooking");
+        modifyVehicleAvailability(booking);
 
         return newBooking;
     }
@@ -55,18 +72,21 @@ public class BookingService {
 
     @Transactional
     public int cancelBooking(int bookingId) {
-        //TODO: first we have to fetch current logged in user id then we have to fetch the particular booking of that user
-        int currentUserId = 1;
+        // fetch current logged in user id then, fetch the particular booking of that user
+        int currentUserId = applicationUserDetailsService.getCurrentLoggedInUser().getId();
+        Optional<Booking> currentBookingOptional = bookingRepository.findByUserIdAndId(currentUserId,bookingId);
 
-        boolean bookingPresent = bookingRepository.findByUserIdAndId(currentUserId, bookingId).isPresent();
-        if (!bookingPresent) throw new IllegalStateException("Couldn't found booking");
+        // check if specified booking exists
+        boolean bookingPresent = currentBookingOptional.isPresent();
+        if (!bookingPresent) throw new IllegalStateException("Couldn't find requested booking");
 
-        //TODO: we need to delete all passenger from the passenger table then we can delete our booking
-        passengerService.deleteAllPassengerByBookingId(bookingId);
+        Booking currentBooking = currentBookingOptional.get();
 
-        //TODO: have to modify available vehicle sit
+        // modifying vehicle capacity
+        modifyVehicleCapacity(currentBooking, "deleteBooking");
+        modifyVehicleAvailability(currentBooking);
 
-        //TODO: we can delete booking
+        // delete booking
         bookingRepository.deleteById(bookingId);
 
         return bookingId;
@@ -75,23 +95,51 @@ public class BookingService {
 
     @Transactional
     public Booking editBooking(int bookingId, Booking updatedBooking) {
-        //TODO: first we have to fetch current logged in user id then we have to fetch the particular booking of that user
-        int currentUserId = 1;
+        // fetch current logged in user id then, fetch the particular booking of that user
+        int currentUserId = applicationUserDetailsService.getCurrentLoggedInUser().getId();
+        Optional<Booking> currentBookingOptional = bookingRepository.findByUserIdAndId(currentUserId,bookingId);
 
-        Booking booking = bookingRepository.findByUserIdAndId(currentUserId, bookingId).orElse(null);
-        if (booking == null) throw new IllegalStateException("Couldn't found booking");
+        // check if specified booking exists
+        boolean bookingPresent = currentBookingOptional.isPresent();
+        if (!bookingPresent) throw new IllegalStateException("Couldn't find requested booking");
 
-        // we only give the permission to change from date and get date
-        booking.setFromDate(updatedBooking.getFromDate());
-        booking.setToDate(updatedBooking.getToDate());
+        Booking currentBooking = currentBookingOptional.get();
 
-        bookingRepository.save(booking);
+        // we only give the permission to change from date and to date
+        currentBooking.setFromDate(updatedBooking.getFromDate());
+        currentBooking.setToDate(updatedBooking.getToDate());
 
-        return booking;
+        bookingRepository.save(currentBooking);
+
+        return currentBooking;
     }
 
+    public void modifyVehicleCapacity(Booking booking, String operation){
+        Vehicle vehicle = booking.getVehicle();
+        int vehicleId = vehicle.getId();
+        int vehicleCapacity = vehicle.getCapacity();
+        int noOfPassangersInBooking = booking.getNumberOfPassanger();
+        if (operation == "deleteBooking"){
+            vehicle.setCapacity(vehicleCapacity + noOfPassangersInBooking);
+        }
+        else if(operation == "createBooking"){
+            vehicle.setCapacity(vehicleCapacity - noOfPassangersInBooking);
+        }
+        vehicleService.editVehicle(vehicleId,vehicle);
+    }
 
+    public void modifyVehicleAvailability(Booking booking){
+        Vehicle vehicle = booking.getVehicle();
+        int vehicleId = vehicle.getId();
+        int vehicleCapacity = vehicle.getCapacity();
+        if (vehicleCapacity == 0 ){
+            vehicle.setAvailableStatus("Unavailable");
+        }
 
-
+        else if (vehicleCapacity > 0) {
+            vehicle.setAvailableStatus("Available");
+        }
+        vehicleService.editVehicle(vehicleId,vehicle);
+    }
 
 }
