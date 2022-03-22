8a4a63adaace812b30edce03f3b1ad9b6875e640
CodeWithSouma <souma.hitech@gmail.com>
2022-03-23 04:31:33 +0530

    add swagger

--- springapp/src/main/java/com/examly/springapp/security/SecurityConfiguration.java
+++ springapp/src/main/java/com/examly/springapp/security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -71,6 +72,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         .exposedHeaders("*");
             }
         };
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**",
+                "/v3/api-docs/**",
+                "/swagger-ui/**");
     }
 
 }
